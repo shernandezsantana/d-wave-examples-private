562f682f994bc392b481c73015383ddbecc22115
Senaida Hernández Santana <shernandezsantana@protonmail.com>
2022-04-18 15:42:18 +0000

    update files

---
+++ knapsack/knapsack-CQM_copy.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+ File name: knapsack-CQM.py
+ 
+ File type: Python file
+ 
+ Author: Jorge Luis Hita
+ 
+ Institution: Private use
+              
+ Date: 1st February 2022
+
+ Description: This program solves naïve instances of the knapsack 
+     problem.
+
+     In a knapsack problem there is a set of objects. Each of them has a
+     value and a weight. A knapsack problem is a problem of selecting a subset 
+     of objects to maximize the total value of the selection, subject to the 
+     restriction that the sum of weights must not exceed some upper value.
+
+     In this file, the Constrained Quadratic Model sampler (CQM) is used.
+
+ Disclaimer: This code is an experimental, not-optimized, not-tested code.
+     USE IT AT YOUR OWN RISK.
+
+"""
+
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+
+from dwave.system import LeapHybridCQMSampler
+from dimod import ConstrainedQuadraticModel, Binary, Integer, quicksum
+
+def test_max_weight(solution, list_of_weights, max_weight):
+    """
+    This function checks whether the inequality constraint is fulfilled
+    """
+    if (np.array(solution)*np.array(list_of_weights)).sum() <= max_weight:
+        print(' \nInequality constraint is fulfilled.\n')
+    else:
+        print(' \nWarning: Inequality constraint is NOT fulfilled.\n')
+
+
+def main():
+
+    # Defines the objects
+    list_of_objects = ["obj_1", "obj_2", "obj_3", "obj_4"]
+
+    # Defines the number of elements for each object
+    list_of_elements = [3, 5, 5, 4]
+
+    # Defines the value for each object
+    list_of_values = [1.3, 2.5, 0.8, 1.7]
+
+    # Defines the nweights for each object
+    list_of_weights = [2, 4, 1.8, 1.6]
+
+    # Defines the upper value of the weights of the subset
+    max_weight = 7.8
+
+    # Number of objects
+    n_objects = len(list_of_objects)
+
+    # Defines the array of binary variables
+    x = [Integer(list_of_objects[object], upper_bound=list_of_elements[object]) for object in range(n_objects)]
+
+    # Declares the cqm object
+    cqm = ConstrainedQuadraticModel()
+
+    # Defines the objective function
+    cqm.set_objective(quicksum(-x[object]*list_of_values[object] for object in range(n_objects)))
+
+    # Defines the inequality constraint: the sum of the weights must not exceed max_weight
+    cqm.add_constraint(quicksum(x[object]*list_of_weights[object] for object in range(n_objects)) <= max_weight, label='max_weight')
+
+    # Declare the sample to be used and solve the problem
+    sampler = LeapHybridCQMSampler()
+    sampleset = sampler.sample_cqm(cqm, time_limit=10)
+
+    # Gets rid of the unfeasible solutions and gets the best feasible solution
+    feasible_sols = np.where(sampleset.record.is_feasible==True)
+    for sol_idx in range(10):
+        solution = sampleset.record[feasible_sols[0][sol_idx]][0]
+
+        # Cheks the inequality constraint
+        test_max_weight(solution, list_of_weights, max_weight)
+
+        # Reshapes the final solution
+        df_sol = pd.DataFrame({'# of elements':list_of_elements, 'value':list_of_values, 
+                            'weight':list_of_weights, '# of elements in solution':solution}, index=list_of_objects)
+
+        # Prints final solution
+        print(df_sol)
+    
+	
+if __name__=="__main__":
+    main()
--- truck/truck_CQM.py
+++ truck/truck_CQM.py
@@ -1,1 +1,99 @@
-from dimod import ConstrainedQuadraticModel
+# import model
+from dimod import ConstrainedQuadraticModel, quicksum, Binary
+from dwave.system import LeapHybridCQMSampler
+import numpy as np
+import random
+import itertools
+
+# problem set up
+num_packages = 300
+
+# priority of each package, 3 = high priority, 1 = low priority
+priority = [random.choice([1,2,3])  for i in range(num_packages)]
+
+# number of days since each package was ordered
+days_since_order = [random.choice([0,1,2,3])  for i in range(num_packages)]
+
+# weight of each package
+cost = [random.randint(1,100)  for i in range(num_packages)]
+
+# maximum weight and number of packages the truck can handle
+max_weight = 3000
+max_parcels = 100
+
+# weights for the objective function
+obj_weight_priority = 1.0
+obj_weight_days = 1
+
+num_items = len(cost)
+
+# build the CQM
+cqm = ConstrainedQuadraticModel()
+
+# create the binary variables
+bin_variables = [Binary(i) for i in range(num_items)]
+
+# - - - - - - - - - - Objective functions - - - - - - - - - -
+# build an objective to consider priority shipping
+objective1 = - obj_weight_priority * quicksum(priority[i] * bin_variables[i] for i in range(num_items))
+
+# build an objective to consider number of days since the order was placed
+objective2 = - obj_weight_days * quicksum(days_since_order[i] * bin_variables[i] for i in range(num_items))
+
+# add the objectives to the CQM
+cqm.set_objective(objective1 + objective2)
+
+# - - - - - - - - - - Constraints - - - - - - - - - -
+# add the maximum capacity constraint
+cqm.add_constraint(quicksum(cost[i] * bin_variables[i] for i in range(num_items)) <= max_weight, label='max_capacity')
+
+# add the maximum parcel (or truck size) constraint
+cqm.add_constraint(quicksum(bin_variables[i] for i in range(num_items)) == max_parcels, label='max_parcels')
+
+# - - - - - - - - - - Submit to the CQM sampler - - - - - - - - - -
+cqm_sampler = LeapHybridCQMSampler()
+sampleset = cqm_sampler.sample_cqm(cqm, time_limit=10)
+print(sampleset.info)
+
+# - - - - - - - - - - Process the results - - - - - - - - - -
+feasible_sols = np.where(sampleset.record.is_feasible == True)
+print(feasible_sols)
+
+if len(feasible_sols[0]):
+    first_feasible_sol = np.where(sampleset.record[feasible_sols[0][0]][0] == 1)
+
+    # characterize the problem
+    problem_array = np.zeros((3,4)).astype(int)
+    for i in range(num_items):
+        problem_array[-1 * (priority[i]-3)][-1 * (days_since_order[i]-3)] += 1
+
+    print("\n********** PROBLEM ********** ")
+    print('            Days since order was placed')
+    print('{:>5s}{:>5s}{:^5s}{:^5s}{:^5s}'.format('Priority |','3','2','1','0'))
+    print('-' * 40)
+
+    for i in range(3):
+        print('{:>5s}{:>10s}{:^5s}{:^5s}{:^5s}'.format(str(-1*(i-3)),str(problem_array[i][0]),str(problem_array[i][1]),
+        str(problem_array[i][2]),str(problem_array[i][3])))
+
+    # characterize the solution
+    solution_array = np.zeros((3,4)).astype(int)
+    total_weight = 0
+    total_items = len(first_feasible_sol[0])
+    for i in range(num_items):
+        if i in first_feasible_sol[0]:
+            solution_array[-1 * (priority[i]-3)][-1 * (days_since_order[i]-3)] += 1
+            total_weight += cost[i]
+
+    print("\n********** SOLUTION ********** ")
+    print('            Days since order was placed')
+    print('{:>5s}{:>5s}{:^5s}{:^5s}{:^5s}'.format('Priority |','3','2','1','0'))
+    print('-' * 40)
+
+    for i in range(3):
+        print('{:>5s}{:>10s}{:^5s}{:^5s}{:^5s}'.format(str(-1*(i-3)),str(solution_array[i][0]),str(solution_array[i][1]),
+        str(solution_array[i][2]),str(solution_array[i][3])))
+
+    print("\nTotal number of selected items:",total_items)
+    print("Total weight of selected items:",total_weight)
+    
